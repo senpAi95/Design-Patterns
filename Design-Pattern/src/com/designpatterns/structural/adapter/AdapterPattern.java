c46c3f4afdd2e4727614fcaf69bf1ca66545f154
Shiva Teja Mummadi <shivateja95@gmail.com>
2021-03-11 00:30:16 -0500

    java doc

--- Design-Pattern/src/com/designpatterns/structural/adapter/AdapterPattern.java
+++ Design-Pattern/src/com/designpatterns/structural/adapter/AdapterPattern.java
@@ -2,6 +2,10 @@ package com.designpatterns.structural.adapter;
 
 import java.util.List;
 
+/**
+ * In this example, {@link Employee} and {@link EmployeeDB} is new code. Legacy code is {@link EmployeeCSV} and
+ * {@link EmployeeLdap}. To use legacy code, we use new adapters like {@link EmployeeAdapterCSV} and {@link EmployeeAdapterLdap}.
+ */
 public class AdapterPattern {
     public static void main(String[] args) {
         EmployeeClient client = new EmployeeClient();
